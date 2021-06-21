1599c2faf0a8bbf5d10142b8d8faaff6e0888950
Daniel Gurreck <daniel.gurreck.gonzalez@est.una.ac.cr>
2021-06-21 14:48:11 -0600

    Se crea la carpeta principal

--- Tarea3/Funciones/carpetas.py
+++ Tarea3/Funciones/carpetas.py
@@ -1,9 +1,13 @@
 import os
+import sys
 class carpeta:
     def crearCapeta():
-        directorio = "/repositorio"
+        directorio = 'repositorio'
+        print("crenado")
         try:
+            print("Se creo el repositorio")
             os.mkdir(directorio)
+            print("Se creo el repositorio")
         except OSError:
             print("La creación del directorio %s falló" % directorio)
         else:
--- Tarea3/InicioController.py
+++ Tarea3/InicioController.py
@@ -9,19 +9,19 @@ app = QtWidgets.QApplication([])
 win = uic.loadUi("Controller/Inicio.ui")
 usuarios.login.leerUsuarios()
 def autenficarUsuario(nombre, password):
-        with open('Usuarios/usuarios.txt') as file:
-            data = json.load(file)
-            for usuario in data['usuarios']:
-                if(usuario['nombre']==nombre and usuario['password']==password):
-                    global win
-                    if(nombre == 'admin'):
-                        win.stackedWidget.setCurrentWidget(win.page_adminCreaRepo)
-                    
-                    win.stackedWidget.setCurrentWidget(win.page_userCreaRepo)
-                    print("Autenticado")
-                    break
+    with open('Usuarios/usuarios.txt') as file:
+        data = json.load(file)
+        for usuario in data['usuarios']:
+            if(usuario['nombre']==nombre and usuario['password']==password):
+                global win
+                if(nombre == 'admin'):
+                    win.stackedWidget.setCurrentWidget(win.page_adminCreaRepo)
                 else:
-                    print("No autenticado")
+                    win.stackedWidget.setCurrentWidget(win.page_userCreaRepo)
+                print("Autenticado")
+                break
+            else:
+                print("No autenticado")
 
 win.btn_registrar.clicked.connect(lambda: win.stackedWidget.setCurrentWidget(win.page_registro))
 win.btn_registrar.clicked.connect(lambda: win.stackedWidget.setCurrentWidget(win.page_registro))
@@ -29,6 +29,7 @@ win.btn_atrasRegistro.clicked.connect(lambda: win.stackedWidget.setCurrentWidget
 win.btn_registrarse.clicked.connect(lambda: usuarios.login.registrarUsuario(win.text_usuarioRegistro.text(),win.text_passwordRegistro.text()))
 win.btn_iniciarSesion.clicked.connect(lambda: autenficarUsuario(win.text_usuario.text(),win.text_password.text()))
 
+win.btn_crearRepoPrincipal.clicked.connect(lambda:carpeta.carpeta.crearCapeta())
 win.show()
 
 sys.exit(app.exec())
